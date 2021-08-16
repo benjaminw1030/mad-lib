d508d67511f07f4b211b18c6e29bab2ce67563da
Ben Wilson <benjaminw1030@gmail.com>
2021-08-16 11:54:36 -0700

    update to add a loop to capitalize text Co-authored-by: Becket Harvey <becket.fc@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,33 @@
-$(document).ready(function() {
-  $("#formOne").submit(function(event) {
+$(document).ready(function () {
+  $("#formOne").submit(function (event) {
     event.preventDefault();
     const person1Input = $("input#person1").val();
     const person2Input = $("input#person2").val();
-    const nounInput= $("input#noun").val();
+    const animalInput = $("input#animal").val();
     const exclamationInput = $("input#exclamation").val();
     const verbInput = $("input#verb").val();
-    const actionInput = $("input#action").val();
+    const nounInput = $("input#noun").val();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".noun").text(nounInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".action").text(actionInput);
+    const inputArray = [
+      person1Input,
+      person2Input,
+      animalInput,
+      exclamationInput,
+      verbInput,
+      nounInput,
+    ];
+
+    let allCaps = [];
+    inputArray.forEach(function (element) {
+      allCaps.push(element.toUpperCase());
+    });
+
+    $(".person1").text(allCaps[0]);
+    $(".person2").text(allCaps[1]);
+    $(".animal").text(allCaps[2]);
+    $(".exclamation").text(allCaps[3]);
+    $(".verb").text(allCaps[4]);
+    $(".noun").text(allCaps[5]);
 
     $("#story").show();
   });
